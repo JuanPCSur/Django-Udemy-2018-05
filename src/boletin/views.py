c227baa605f7a2823136672bfc4d673c0517d95a
Juan PC <34580931+JuanPCSur@users.noreply.github.com>
2018-05-14 01:46:19 +0200

    Add files via upload

---
+++ src/boletin/views.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from .forms import RegForm
+# Create your views here.
+def inicio(request):
+    form= RegForm()
+    contex = {
+        "el_form": form,
+    }
+    return render(request, "inicio.html",contex)
